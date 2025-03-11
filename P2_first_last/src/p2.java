9dc41a11e6e23e123dc304b575df83f6db33140f
1962991 <1962991@01-18121.pusd.dom>
2025-03-11 14:02:13 -0700

    3.11

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class p2 {
-
+	public static Scanner scanner;
+	public int rows, cols, maps;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("p2");
+		File file = new File(H:\\git\ds-project-2-ellioth17\\P2_first_last\\src\\TestMaze01);
+		readMap("TestMaze01")
+	}
+	public static void readMap(String filename) {
+		try {
+			File file = new File(filename);
+			Scanner scanner = new Scanner(file);
+			int numRows = scanner.nextInt();
+			int numCols = scanner.nextInt();
+			int numRooms = scanner.nextInt();
+		}
+		catch(FileNotFoundException e) {
+			System.out.println();
+		}
 	}
 
 }
