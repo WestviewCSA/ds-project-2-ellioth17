dbd1c8fd326d791deb1028edb197f9eccdd46722
1962991 <1962991@01-18121.pusd.dom>
2025-03-12 13:49:58 -0700

    3.12 2nd

---
+++ P2_first_last/src/Tile.java
@@ -0,0 +1,48 @@
+public class Tile {
+		private int row, col;
+		private char type;
+		private boolean visited;
+		private String output;
+		
+		public Tile(int row, int col, char type, String output) {
+			super();
+			this.row = row;
+			this.col = col;
+			this.type = type;
+			this.visited = false;
+			this.output = output;
+		}
+		public int getRow() {
+			return row;
+		}
+		public void setRow(int row) {
+			this.row = row;
+		}
+		public int getCol() {
+			return col;
+		}
+		public void setCol(int col) {
+			this.col = col;
+		}
+		public char getType() {
+			return type;
+		}
+		public void setType(char type) {
+			this.type = type;
+		}
+		
+		public boolean getVisited() {
+			return visited;
+		}
+		public void setVisited(boolean visited) {
+			this.visited = visited;
+		}
+		
+		public String toString() {
+			return output;
+		}
+}
+
+
+
+
