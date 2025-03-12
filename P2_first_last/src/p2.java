bdbd4eaabe7423f6d819155b653e3edefa7ac778
1962991 <1962991@01-18121.pusd.dom>
2025-03-12 13:20:48 -0700

    3.12 first

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,24 +1,72 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 public class p2 {
-	public static Scanner scanner;
-	public int rows, cols, maps;
+	private static Scanner scanner;
+	private int rows, cols, maps;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File file = new File(H:\\git\ds-project-2-ellioth17\\P2_first_last\\src\\TestMaze01);
-		readMap("TestMaze01")
+	    String filename = "H:\\git\\ds-project-2-Nishk04-1\\P2_first_last\\src\\TestMaze01";
+		readMap("TestMaze01");
 	}
+	
 	public static void readMap(String filename) {
 		try {
 			File file = new File(filename);
 			Scanner scanner = new Scanner(file);
+			
 			int numRows = scanner.nextInt();
 			int numCols = scanner.nextInt();
 			int numRooms = scanner.nextInt();
-		}
-		catch(FileNotFoundException e) {
-			System.out.println();
+			
+			int rowIndex = 0;
+			//process the map!
+			while(scanner.hasNextLine()) {
+				String row = scanner.nextLine();
+				
+				if(row.length()>0) {
+					for(int col = 0; col < numCols && col < row.length(); col++) {
+						char element = row.charAt(col);
+						Tile obj = new Tile(rowIndex, col, element);
+					}
+				}
+			}
+			
+		} catch (FileNotFoundException e){
+			System.out.println(e);
 		}
 	}
-
+	
+	public static void readCoordinateMap(String filename) {
+		try {
+			File file = new File(filename);
+			Scanner scanner = new Scanner(file);
+			
+			int numRows = scanner.nextInt();
+			int numCols = scanner.nextInt();
+			int numRooms = scanner.nextInt();
+			
+			int rowIndex = 0;
+			//process the map!
+			while(scanner.hasNextLine()) {
+				String row = scanner.nextLine();
+				//Find first char --> store as the element type for all
+				//then
+				if(row.length()>0) {
+					for(int col = 0; col < numCols && col < row.length(); col++) {
+						char element = row.charAt(col);
+						Tile obj = new Tile(rowIndex, col, element);
+					}
+				}
+			}
+			
+		} catch (FileNotFoundException e){
+			System.out.println(e);
+		}
+	}
+	
 }
+
+
+
